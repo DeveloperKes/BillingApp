890297cb90bde1b34a99b543828342640a3157cf
DeveloperKes <kevin.sanchez8117@gmail.com>
2025-09-29 02:13:41 -0500

    feat: enhance invoice management with resolvers, search functionality, and form improvements

--- src/app/pages/invoices/search-invoices/search-invoices.component.ts
+++ src/app/pages/invoices/search-invoices/search-invoices.component.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { SelectorClientComponent } from '../../clients/elements/selector-client/selector-client.component';
 import { FormsModule } from '@angular/forms';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { InvoiceService } from '../services/invoice.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, of, Subject } from 'rxjs';
+import { ListResponse } from '../../../shared/interfaces/http';
+import { Invoice } from '../interfaces/Invoice';
 
 @Component({
   selector: 'app-search-invoices',
-  imports: [SelectorClientComponent, FormsModule, CurrencyPipe],
+  imports: [SelectorClientComponent, FormsModule, CurrencyPipe, DatePipe],
   templateUrl: './search-invoices.component.html',
   styleUrl: './search-invoices.component.scss'
 })
@@ -14,15 +18,37 @@ export class SearchInvoicesComponent {
   public clientId = signal<number>(0);
   public invoiceNumber: number = 0;
 
-  public invoices = signal<any[]>([]);
+  public invoices = signal<Invoice[]>([]);
+  public typeSearch = "client";
 
-  constructor(private readonly _invoices: InvoiceService) { }
+  constructor(
+    private readonly _invoices: InvoiceService,
+    private readonly _active: ActivatedRoute
+  ) {
+    _active.data.pipe(
+      map(data => (data['invoices'] ?? []) as ListResponse<Invoice>)
+    ).subscribe({
+      next: (data) => {
+        const { code, data: { results } } = data;
+        if (code == 0) {
+          this.invoices.set(results);
+        }
+      }
+    })
+  }
 
   search() {
-    this._invoices.getAllInvoices().subscribe({
-      next: (data)=>{
-        console.log("Data invoices", data);
-        
+    let sub = new Observable<ListResponse<Invoice>>;
+    if (this.typeSearch == "client" && this.clientId() != 0) sub = this._invoices.getInvoiceByClient(this.clientId());
+    else if (this.typeSearch == "number" && this.invoiceNumber > 0) sub = this._invoices.getInvoiceByNumber(this.invoiceNumber);
+
+    sub.subscribe({
+      next: (data) => {
+        console.log(data);
+        const { code, data: { results } } = data;
+        if (code == 0) {
+          this.invoices.set(results);
+        }
       }
     })
   }
